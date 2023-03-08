4130cc69d1f44b0e031c5ceae6dc9e06a37a8f40
murat-karasah <64283533+murat-karasah@users.noreply.github.com>
2023-03-09 00:18:44 +0300

    Add files via upload

---
+++ rammonitor/README.md
@@ -0,0 +1,10 @@
+# rammonitor
+This program is a .NET Console Application designed to list all running processes on a Windows system and their corresponding memory usage. It uses the System.Diagnostics library to get a list of running processes and loops through each process to retrieve its details.
+
+The program displays the name, ID, and memory usage (in kilobytes) for each process. It also attempts to retrieve the base address and size of the main module for each process, and lists the name, base address, and size of all modules in the process.
+
+If there is an exception while retrieving the main module details for a process, the program catches the exception and displays an error message.
+
+This program can be useful for monitoring memory usage and identifying processes that are using excessive amounts of memory. It also provides detailed information about each process and its modules.
+
+To run the program, simply compile and execute it. The program will display the results in the console window.
---
+++ rammonitor/ramMonitor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+Process[] processes = Process.GetProcesses();
+
+foreach (Process process in processes)
+{
+    Console.WriteLine("------------------------");
+    Console.WriteLine("Process Name: {0} \t ID: {1} \t Memory Usage: {2} KB",
+        process.ProcessName, process.Id, process.WorkingSet64 / 1024);
+
+    try
+    {
+        ProcessModule mainModule = process.MainModule;
+        IntPtr baseAddress = mainModule.BaseAddress;
+        int moduleSize = mainModule.ModuleMemorySize;
+
+        Console.WriteLine("Base Address: 0x{0:X}", baseAddress.ToInt64());
+        Console.WriteLine("Module Size: {0} bytes", moduleSize);
+
+        foreach (ProcessModule module in process.Modules)
+        {
+            Console.WriteLine("Module Name: {0}", module.ModuleName);
+            Console.WriteLine("Base Address: 0x{0:X}", module.BaseAddress.ToInt64());
+            Console.WriteLine("Module Size: {0} bytes", module.ModuleMemorySize);
+            Console.WriteLine();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: {0}", ex.Message);
+    }
+
+    Console.WriteLine("------------------------");
+}
+
+Console.ReadLine();
